c136493bfa6f8728ffbd2efc6a4617126a2cd59f
Andrew Soback <sobacka@oregonstate.edu>
2018-08-21 15:16:11 -0500

    Changed dbcon
    Updated info for gearhost live db/site

--- app/bin/dbcon.js
+++ app/bin/dbcon.js
@@ -5,10 +5,10 @@ const mysql = require('mysql');
 const dbs = {
   // for use with production db.
   production: {
-    host: 'host',
-    user: 'username',
-    password: 'password',
-    database: 'database',
+    host: 'den1.mysql4.gear.host',
+    user: 'mealwheel',
+    password: 'Fl2bd0~S4Z~G',
+    database: 'mealwheel',
   },
   // for use with local mySQL db.
   development: {
